9349a6ddf6a333c048f5012e8b93d8ecab7af982
thomh14 <haugene93@live.no>
2019-11-19 14:57:31 +0100

    database 9/11 simulation

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const cors = require('cors'); //when the clients aren't on the server
 const app = express(); //server-app
 const userRoutes = require("./routs/user.js");
+const pg = require('pg');
+const dbURI = "postgres://uuvcwgoifcnizu:2316c7908138ef053a96645eb6abd39e84e8b36b86f73309cee43f32b4533110@ec2-54-247-92-167.eu-west-1.compute.amazonaws.com:5432/ddov05lflkkq5f";
+const connstring = process.env.DATABASE_URL || dbURI;
+const pool = new pg.Pool ({ connectionString: connstring });
 
 
 const DEFAULT_PORT = 8000;
+
+
 
 // middleware ------------------------------------
 app.set('port', (process.env.PORT || DEFAULT_PORT));
@@ -16,8 +22,16 @@ app.use("/users", userRoutes);
 // -----------------------------------------------
 
 // endpoint GET ----------------------------------
-app.get('/', function (req, res) {
-    // code here...
+app.get('/users',  async function (req, res) {
+
+    let sql = 'SELECT * from users';
+    try {
+      let result = await pool.query(sql);
+      res.status(200).json(result.rows);
+    }
+    catch(err) {
+      res.status(500).json({error: err});
+    }
 
     res.status(200).send("hello from GET"); //send response
 });
