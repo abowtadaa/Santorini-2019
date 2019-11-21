ce356d864e2501ed9eb51f6eec261f67fdc65955
abowtadaa <johannes.nystad@hotmail.com>
2019-11-21 14:18:01 +0100

    Merge pluss game fix

--- routs/game.js
+++ routs/game.js
@@ -3,6 +3,7 @@ const route = express.Router();
 
 let games = [];
 
+
 app.get("/game/:gameId", function (req,res) {
     for(game og games){
         if(req.params.gameId===game.id){
@@ -12,3 +13,23 @@ app.get("/game/:gameId", function (req,res) {
         }
     }
 })
+
+app.post("/game", function (req,res){
+    console.log(req.body);
+})
+
+
+function santoriniGame (host,gameBoard) {
+    this.host = host
+    this.secondPlayer = null;
+    this.gameBoard = gameBoard;
+
+    this.playerJoined = function (player) {
+        this.secondPlayer = playerM;
+    }
+
+    this.gameBoardChange = function (tile,pos) {
+        this.gameBoard.tile[pos.y][pos.x]=tile;
+        return this.gameBoard;
+    }
+}
