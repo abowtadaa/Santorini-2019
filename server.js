659966fe9021d403654c837aec0d8c6b0fd45f9a
abowtadaa <johannes.nystad@hotmail.com>
2019-11-21 12:03:53 +0100

    Server lager brukere

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const bcryptjs = require('bcryptjs');
 const express = require('express');
 const cors = require('cors'); //when the clients aren't on the server
 const app = express(); //server-app
-const userRoutes = require("./routs/user.js");
+//const userRoutes = require("./routs/user.js");
 const pg = require('pg');
 const dbURI = "postgres://uuvcwgoifcnizu:2316c7908138ef053a96645eb6abd39e84e8b36b86f73309cee43f32b4533110@ec2-54-247-92-167.eu-west-1.compute.amazonaws.com:5432/ddov05lflkkq5f" + "?ssl=true";
 const connstring = process.env.DATABASE_URL || dbURI;
@@ -19,7 +19,7 @@ app.set('port', (process.env.PORT || DEFAULT_PORT));
 app.use(cors()); //allow all CORS requests
 app.use(express.json()); //for extracting json in the request-body
 app.use('/', express.static('public')); //for serving client files
-app.use("/users", userRoutes);
+//app.use("/users", userRoutes);
 
 // -----------------------------------------------
 
@@ -29,7 +29,7 @@ app.get('/users', async function (req, res) {
   let sql = 'SELECT * from users';
   try {
     let result = await pool.query(sql);
-    console.log(result);
+    //console.log(result);
     res.status(200).json(result.rows);
 
   }
@@ -42,12 +42,13 @@ app.get('/users', async function (req, res) {
 
 // endpoint POST ---------------------------------
 app.post('/users', async function (req, res) {
+    console.log(req.body);
 
   let updata = req.body;
 
   let hash = bcryptjs.hashSync(updata.password, 10);
 
-  let sql = 'INSERT INTO users (id, email, username, pswhash) VALUES(DEFAULT, $1, 2$, 3$) RETURNING *';
+  let sql = 'INSERT INTO users (id, email, username, pswhash) VALUES(DEFAULT, $1, $2, $3) RETURNING *';
   let values = [updata.email, updata.username, hash];
 
   try {
